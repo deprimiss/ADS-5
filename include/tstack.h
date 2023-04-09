b063d96ec950c3c19ac714274b965e5f5021d161
deprimiss <125207464+deprimiss@users.noreply.github.com>
2023-04-09 18:07:30 +0300

    Update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -4,7 +4,44 @@
 
 template<typename T, int size>
 class TStack {
-  // добавьте код стека
+private:
+    T* array;
+    int top;
+
+public:
+    TStack() : top(-1) {
+        array = new T[size];
+    }
+    bool isEmpty() const {
+        return top == -1;
+    }
+    bool isFull() const {
+        return top == size - 1;
+    }
+    void push(T num) {
+        if (isFull()) {
+            throw std::string("full!");
+        }
+        else {
+            array[++top] = num;
+        }
+    }
+    const T& get() {
+        if (isEmpty()) {
+            throw std::string("empty!");
+        }
+        else {
+            return array[top];
+        }
+    }
+    const T& pop() {
+        if (isEmpty()) {
+            throw std::string("empty!");
+        }
+        else {
+            return array[top--];
+        }
+    }
 };
 
 #endif  // INCLUDE_TSTACK_H_
